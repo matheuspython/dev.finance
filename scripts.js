0506bcdb6497f3fd40df5eba17bec7d77d66f7f6
matheus souza <ruivorobado@gmail.com>
2021-01-30 14:08:00 -0300

    adicionando funcoes para colocar os elementos na tabela dinamicamente

--- scripts.js
+++ scripts.js
@@ -9,3 +9,55 @@ const Modal = {
         classList.remove('active')
     }
 }
+
+const Transections = [{
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021'
+    }, {
+        id: 2,
+        description: 'Website',
+        amount: -500000,
+        date: '23/01/2021'
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021'
+    }
+]
+
+const Transection = {
+    incomes() {
+
+    },
+    expenses() {
+
+    },
+    total() {
+
+    }
+}
+
+const DOM = {
+    addTransection(transection, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransection(transection)
+        console.log(tr.innerHTML)
+    },
+    innerHTMLTransection(transection) {
+        const html = `
+            <td class="description">${transection.description}</td>
+            <td class="expense"> ${transection.amount}</td>
+            <td class="date">${transection.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover transação">
+            </td>
+        `
+        return html
+    }
+}
+
+DOM.addTransection(Transections[0])
