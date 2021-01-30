23652a2cb375d74d42f255460c2a7af1b4bcb940
matheus souza <ruivorobado@gmail.com>
2021-01-30 12:10:34 -0300

    adicionando arquivo de scripts

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const Modal = {
+    open() {
+        document.querySelector('.modal-overlay').
+        classList.add('active')
+    },
+    close() {
+
+        document.querySelector('.modal-overlay').
+        classList.remove('active')
+    }
+}
