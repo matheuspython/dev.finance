8288c970a4d1669e207b5884b79051fc6de5f917
matheus souza <ruivorobado@gmail.com>
2021-01-30 23:55:17 -0300

    funcionalidade de adicionar novos itens a tabela concluida e iniciando a de remover

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const Modal = {
     open() {
         document.querySelector('.modal-overlay').
         classList.add('active')
+        document.querySelector('#description').focus()
     },
     close() {
 
@@ -9,8 +10,6 @@ const Modal = {
         classList.remove('active')
     }
 }
-
-
 
 const Transaction = {
     all: [{
@@ -65,23 +64,23 @@ const Transaction = {
 
 const DOM = {
     TransectionsContainer: document.querySelector('#data-table tbody'),
-    addTransection(transection, index) {
+    addTransaction(transaction, index) {
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransection(transection)
-
+        tr.innerHTML = DOM.innerHTMLTransection(transaction, index)
+        tr.dataset.index = index
         DOM.TransectionsContainer.appendChild(tr)
     },
-    innerHTMLTransection(transection) {
-        const CSSclass = transection.amount > 0 ? 'income' : 'expense'
+    innerHTMLTransection(transaction, index) {
+        const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
 
-        const amount = Utils.formatCurrency(transection.amount)
+        const amount = Utils.formatCurrency(transaction.amount)
 
         const html = `
-            <td class="description">${transection.description}</td>
+            <td class="description">${transaction.description}</td>
             <td class="${CSSclass}"> ${amount}</td>
-            <td class="date">${transection.date}</td>
+            <td class="date">${transaction.date}</td>
             <td>
-                <img src="./assets/minus.svg" alt="Remover transação">
+                <img onclick="transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
             </td>
         `
         return html
@@ -102,6 +101,11 @@ const DOM = {
 }
 
 const Utils = {
+    formatAmount(value) { return Number(value) * 100 },
+    formatDate(date) {
+        const splittedDate = date.split('-')
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+    },
     formatCurrency(value) {
         const signal = Number(value) < 0 ? '-' : ''
         value = String(value).replace(/\D/g, '')
@@ -111,26 +115,76 @@ const Utils = {
             currency: "BRL"
         })
         return signal + value
-    }
+    },
+
 }
 
 const Form = {
-    formatData() {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+    getValues() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
 
+    formatData() {
+        console.log('formatar dados')
     },
-    validateField() {
-        console.log(Form)
+
+    validateFields() {
+        const { description, amount, date } = Form.getValues()
+
+        if (description.trim() === "" || amount.trim() === "" || date.trim() === "") {
+            throw new Error('por favor, preencha todos os campos')
+        }
     },
+
+    formatValues() {
+        let { description, amount, date } = Form.getValues()
+        amount = Utils.formatAmount(amount)
+
+        date = Utils.formatDate(date)
+
+        return { description, amount, date }
+    },
+    saveTransaction(transaction) {
+        Transaction.add(transaction)
+    },
+    clearFields() {
+        Form.description.value = ''
+        Form.amount.value = ''
+        Form.date.value = ''
+    },
+
     submit(event) {
         event.preventDefault()
-        Form.validateField()
-        Form.formatData()
+
+        try {
+            Form.validateFields()
+
+            const transaction = Form.formatValues()
+
+            Form.saveTransaction(transaction)
+
+            Form.clearFields()
+            Modal.close()
+
+
+        } catch (error) {
+            alert(error)
+        }
+
+
     }
 }
 
 const App = {
     init() {
-        Transaction.all.forEach(transection => DOM.addTransection(transection))
+        Transaction.all.forEach(DOM.addTransaction)
 
 
         DOM.updateBalance()
